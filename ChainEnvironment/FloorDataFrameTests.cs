a40d50c3fc77fedd724376bb485a86dfa3f11364
峻也 本田 <morlyckbd@gmail.com>
2022-04-04 17:37:01 +0900

    FloorDataFrameのテストクラス追加

---
+++ ChainEnvironment/FloorDataFrameTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommonElement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonElement.Tests
+{
+    #region(FloorDataFrameのシリアライズ/デシリアライズ)
+    [TestClass()]
+    public class FloorDataFrameTests
+    {
+        [TestMethod()]
+        public void SerializeTest() {
+            Assert.Fail();
+        }
+    }
+    #endregion
+}
