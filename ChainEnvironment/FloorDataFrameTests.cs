0b8137dcbc4501447c43c8e0b467ba8042b6d8c2
峻也 本田 <morlyckbd@gmail.com>
2022-04-05 16:45:29 +0900

    テスト環境の追加

--- ChainEnvironment/FloorDataFrameTests.cs
+++ ChainEnvironment/FloorDataFrameTests.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json;
+using CommonElement;
+
 namespace CommonElement.Tests
 {
     #region(FloorDataFrameのシリアライズ/デシリアライズ)
@@ -18,4 +21,45 @@ namespace CommonElement.Tests
         }
     }
     #endregion
+}
+
+
+public class GeneralSd : CommonElement.ISerializerAndDeserializer
+{
+    public static GeneralSd Instance { get; } = new GeneralSd();
+
+    public static string SerializeStatic(object obj) {
+        return JsonConvert.SerializeObject(obj);
+    }
+    public static typ DeserializeStatic<typ>(string jsonText) {
+        return JsonConvert.DeserializeObject<typ>(jsonText);
+    }
+
+    public static object DeserializeStatic(Type typ, string jsonText) {
+        return JsonConvert.DeserializeObject(jsonText, typ);
+    }
+
+    //
+    public string Serialize(object obj) {
+        return JsonConvert.SerializeObject(obj);
+    }
+    public typ Deserialize<typ>(string jsonText) {
+        return JsonConvert.DeserializeObject<typ>(jsonText);
+    }
+
+    public object Deserialize(Type typ, string jsonText) {
+        throw new NotImplementedException();
+    }
+
+    public string SerializeCustomOrBasic(object obj) {
+        throw new NotImplementedException();
+    }
+
+    public typ DeserializeCustomOrBasic<typ>(string jsonText) {
+        throw new NotImplementedException();
+    }
+
+    public object DeserializeCustomOrBasic(Type typ, string jsonText) {
+        throw new NotImplementedException();
+    }
 }
